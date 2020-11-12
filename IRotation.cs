0ed5c100ede1aa7c87e61fb1381d25b82fe02b08
PakAleksey <pakaleksey1991@mail.ru>
2020-11-12 19:24:41 +0700

    Begin to HW Lesson6

--- IRotation.cs
+++ IRotation.cs
@@ -1,7 +1,4 @@
-﻿using System;
-
-
-namespace Assets.MyScripts
+﻿namespace Assets.MyScripts
 {
     public interface IRotation
     {
--- InputController.cs
+++ InputController.cs
@@ -6,15 +6,27 @@ namespace Assets.MyScripts
     public sealed class InputController : IExecute
     {
         private readonly PlayerBase _playerBase;
+        private readonly SaveDataRepository _saveDataRepository;
+        private readonly KeyCode _savePlayer = KeyCode.C;
+        private readonly KeyCode _loadPlayer = KeyCode.V;
 
         public InputController(PlayerBase player)
         {
             _playerBase = player;
+            _saveDataRepository = new SaveDataRepository();
         }
 
         public void Execute()
         {
             _playerBase.Move(Input.GetAxis(AxisManager.HORIZONTAL), 0.0f, Input.GetAxis(AxisManager.VERTICAL));
+            if (Input.GetKeyDown(_savePlayer))
+            {
+                _saveDataRepository.Save(_playerBase);
+            }
+            if (Input.GetKeyDown(_loadPlayer))
+            {
+                _saveDataRepository.Load(_playerBase);
+            }
         }
     }
 }
