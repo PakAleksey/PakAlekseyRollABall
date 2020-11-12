d116748a2312dac307ccaeeb69e3f6a1542bdcc1
PakAleksey <pakaleksey1991@mail.ru>
2020-11-12 18:41:49 +0700

    made Crypto class

---
+++ Crypto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.MyScripts
+{
+    public sealed class Crypto
+    {
+        public static string CryptoXOR(string text, int key = 42)
+        {
+            var result = String.Empty;
+            foreach (var simbol in text)
+            {
+                result += (char)(simbol ^ key);
+            }
+            return result;
+        }
+    }
+}
